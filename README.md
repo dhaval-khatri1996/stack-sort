f23d69a6b64afc5b488a1ea7d451b2fcac2b9ff3
dhaval-khatri1996 <dhavalkhatri1996@gmail.com>
2021-01-24 20:07:16 +0530

    code refractor

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # stack-sort
-A new sorting algorithm named stack-sort
+A new sorting algorithm named stack-sort.
+
+The basic princple is to divide and conquer. Each element is either added to current stack or the other stack based on wheather the current element is in order compared to the previous element. If its in order its added to current stack, otherwise the stack is changed and the element is added to new current stack.
+
+
+C++ sort compared to stack sort on Ubuntu 10.04
+1.5 * 10^8 integer elements
+Time in seconds
+        | Stack-sort  | C++ sort
+Random  |     39      |    47
+Sorted  |     1       |    26        
+Reverse |     2       |    23
+
+Note: Results can be optimized by micro-optimizing, like
+    -merging smallest arrays first rather then just recursive merging 
+    -thresshold can be better selected to save time
--- stack-sort.cpp
+++ stack-sort.cpp
@@ -1,141 +1,198 @@
+//Argumnets both optional size if not passedd 10,000,000 is default and -c or -compare to run inbuilt std.
 #include<stdio.h>
-#include <ctime>
+#include<stdlib.h>
+#include<time.h>
 #include<algorithm>
-#include <cstdlib> 
-#include<iostream>
-using namespace std;
-int count=0;
-void actualSort(int *,int *,int *,int *);
-void stackSort(int *start,int *end){
-	int size=(end-start);
-	int *arr2=(int *)calloc((size),sizeof(int));
-	actualSort(start,arr2,end,arr2+size);
-	free(arr2);
+#include<string.h>
+int THRESSHOLD = 14;
+void merge(int *,int *, int *, int *);
+int *partition(int *, int *, int *, int *);
+void stackSort(int *, int *, int *, int *);
+void reverseArray(int *, int *);
+void insertionSort(int *arrayStart,int *arrayEnd)
+{
+    int n = arrayEnd-arrayStart, i, j, temp = 0;
+    for(i = 1 ; i<n ; i++)
+    {
+        temp = *(arrayStart + i);
+        for(j = i-1; j>=0; j--)
+        {
+            if(temp<(*(arrayStart+j)))
+            {
+                *(arrayStart + j + 1) = *(arrayStart + j);
+            }
+            else
+            {
+                break;
+            }
+        }
+        *(arrayStart + j+ 1) = temp;
+    }
 }
-int *divide(int *start1,int *start2,int *end1,int *end2){
-	int *top1=start2,*top2=end2,flag=0,*i=start1;
-	*start2 = *i++;
-
-    /*code to devide the array
-      Working
-      Assume two stacks are made from each end of the array.
-      We start from inserting elements from orignal array to one of the stacks
-      Note: (start from anyone stack of your choice, the code below starts from stack one)
-      Keep adding elements in the current stack from the orignal array until the element on the top
-      is smaller or equal to the element being inserted.(condition 1)
-      If the current element is bigger then the element to be added we add the new element in the other stack, 
-      consider this(stack 2) as our current stack and keep adding elements from the orignal array as long as condition 1 is followed
-      We switch stacks everytime condition 1 is false
-      NOTE : (While switching the stack when ignore condition 1 for that element)
-      the process is done when all elements are pushed into the two stack (or one of them)*/
-
-	while(i<end1){
-        /*Flag indictes the number of times the stack was changed 
-        and helps us keep record of the stack being used*/
-	
-		if(flag%2){
-			if(*i<*top2){
-				*(++top1)=*i;
-				flag++;
-			}else{
-				*(--top2)=*i;
-			}
-		}
-		else{
-			if(*i<*top1){
-				*(--top2)=*i;
-				flag++;
-			}else{
-				*(++top1)=*i;
-			}
-		}
-		i++;
-	}
-	i=start1;
-    /*If flag value is 0/1 this indicates the values in both stack are sorted.*/
-    if(flag<2){             
-		end2--;
-		while(top1>=start2&&top2<=end2){
-			if(*start2<*end2){
-				*i=*start2++;
-			}else{
-				*i=*end2--;
-			}
-			i++;
-		}
-		while(top1>=start2){
-			*i=*start2++;
-			i++;
-		}
-		while(top2<end2){
-			*i=*end2--;
-			i++;
-		}
-		return NULL;
-	}
-	return top1;
-}
-void actualSort(int *start1,int *start2,int *end1,int *end2){
-	if(start1>=end1){                   //1 or 0 elements in the array
-		return;
-	}
-	if((end1-start1)==2){               //2 elements in the array
-		 end1--;
-		if(*end1<*start1){              //Swap only if the first element is bigger the the other
-			int temp = *start1;
-			*start1=*end1;
-			*end1=temp;
-		}
-		return;
-	}
-
-	int *top1=divide(start1,start2,end1,end2);          //return NULL if the array is not sorted
-    
-    if(top1!=NULL){                                     //Sort the array if it is not already sorted
-		int *top2=top1+1,*temp=start1+(top1-start2)+1,*i=start1;
-        /* sorting the smaller sub array first to keep the stack trace small*/
-
-		if((top2-start2)<(end2-top2)){
-			actualSort(start2,start1,top2,temp);
-			actualSort(top2,temp,end2,end1);
-		}else{
-			actualSort(top2,temp,end2,end1);
-			actualSort(start2,start1,top2,temp);
-		}
-
-        //Merging both the sorted array.
-		while(top1>=start2&&top2<end2){
-			if(*start2<*top2){
-					*i=*start2++;
-				}else{
-					*i=*top2++;
-				}
-			i++;
-		}
-		while(top1>=start2){
-			*i=*start2++;
-			i++;
-		}
-		while(top2<end2){
-			*i=*top2++;
-			i++;
-		}
-	}
+void stackSort(int *arrayStart,int *arrayEnd)
+{
+    if(arrayStart>=arrayEnd)
+    {
+        return;
+    }
+    int arraySize=(arrayEnd-arrayStart);
+    //auxilary array is used to divide the data to be sorted
+    int *auxilaryArray=(int *)calloc((arraySize),sizeof(int));
+    stackSort(arrayStart, arrayEnd, auxilaryArray, auxilaryArray+arraySize);
+    free(auxilaryArray);
 }
 
-int main(){
-	int i,*arr,size= 1;
-	printf("Enter the size of array : ");
-	scanf("%d",&size);
-	arr=(int *)calloc(size,sizeof(int));
-	for(i=0;i<size;i++){
-		arr[i]=rand();
-	}
-	clock_t now1,now2;
-	now1=clock();
-	stackSort(arr,arr+size);
-	now2=clock();
-	printf("Time taken by stack-sort to %d sort elemts is %llf seconds.",size,(float)(now2-now1)/(float)CLOCKS_PER_SEC);
-	return 0;
+void stackSort(int * arrayStart, int * arrayEnd, int *auxilaryArrayStart, int *auxilaryArrayEnd)
+{
+    if(arrayStart>=arrayEnd)
+        return;
+
+    if((arrayEnd-arrayStart) < THRESSHOLD)
+        return insertionSort(arrayStart,arrayEnd);
+
+    int *auxilaryArrayMid = partition(arrayStart,arrayEnd,auxilaryArrayStart,auxilaryArrayEnd);
+
+    if(auxilaryArrayMid!=NULL)
+    {
+
+        int *arrayMid = arrayStart + (auxilaryArrayMid - auxilaryArrayStart);
+
+        //auxillary array and origanl array have been swapped now.
+        stackSort(auxilaryArrayStart, auxilaryArrayMid+1, arrayStart, arrayMid+1);
+        stackSort(auxilaryArrayMid+1, auxilaryArrayEnd, arrayMid+1, arrayEnd);
+
+        merge(arrayStart,auxilaryArrayStart,auxilaryArrayMid,auxilaryArrayEnd);
+    }
 }
 
+int * partition(int * arrayStart,int * arrayEnd, int *auxilaryArrayStart, int *auxilaryArrayEnd )
+{
+    if(arrayStart>=arrayEnd)
+        return NULL;
+    int *leftStackTop = auxilaryArrayStart, *rightStackTop = auxilaryArrayEnd, stackChangeCount=0;
+    int *i = arrayStart ;                             //to traverse the array
+
+    *leftStackTop = *i++;  //First element is copied on left stack as we start from left stack
+
+    while(i < arrayEnd)
+    {
+
+        /*if element *i is not in order( greater as in our case) then we change the stack,
+          and put the element on top of the current stack*/
+
+        if(stackChangeCount % 2)
+        {
+            if(*i<*rightStackTop)
+            {
+                *(++leftStackTop)=*i;
+                stackChangeCount++;
+            }
+            else
+            {
+                *(--rightStackTop)=*i;
+            }
+        }
+        else
+        {
+            if(*i<*leftStackTop)
+            {
+                *(--rightStackTop)=*i;
+                stackChangeCount++;
+            }
+            else
+            {
+                *(++leftStackTop)=*i;
+            }
+        }
+        i++;
+    }
+
+    if(stackChangeCount <3)
+    {
+        if(stackChangeCount != 0)  //right stack is sorted but in reverse
+            reverseArray(rightStackTop,auxilaryArrayEnd);
+        else                      // array is already sorted
+            return NULL;
+    }
+    return leftStackTop;
+}
+
+void reverseArray(int *arrayStart, int *arrayEnd)
+{
+    arrayEnd--;
+    int temp;
+    while(arrayStart<arrayEnd)
+    {
+        temp = *arrayStart;
+        *arrayStart = *arrayEnd;
+        *arrayEnd = temp;
+        --arrayEnd;
+        ++arrayStart;
+    }
+    return;
+}
+void merge(int *arrayStart, int *auxilaryArrayStart, int *auxilaryMid,int *auxilaryArrayEnd)
+{
+    int *i = arrayStart,*leftArrayTop = auxilaryArrayStart, *rightArrayStart = auxilaryMid+1;
+    while(auxilaryMid>=leftArrayTop&&rightArrayStart<auxilaryArrayEnd)
+    {
+        if(*leftArrayTop<*rightArrayStart)
+        {
+            *i=*leftArrayTop++;
+        }
+        else
+        {
+            *i=*rightArrayStart++;
+        }
+        i++;
+    }
+    while(auxilaryMid>=leftArrayTop)
+    {
+        *i=*leftArrayTop++;
+        i++;
+    }
+    while(rightArrayStart<auxilaryArrayEnd)
+    {
+        *i=*rightArrayStart++;
+        i++;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int i,*arr1,*arr2,arraySize= 50000000;//100000000;
+    time_t timer1,timer2,timer3,timer4;
+
+if(argc>1){	
+try{
+	arraySize = atoi(argv[1]);
+}catch(...){
+	arraySize = 50000000;
+}
+}
+    printf("Enter the size of array : %d",arraySize);
+    arr1=(int *)calloc(arraySize,sizeof(int));
+    arr2=(int *)calloc(arraySize,sizeof(int));
+    for(i=0; i<arraySize; i++)
+    {
+        *(arr1+i)=rand();
+        *(arr2 + i) = *(arr1+i);
+    }
+    time(&timer1);
+    stackSort(arr1,arr1+arraySize);
+    time(&timer2);
+    printf("\nTime taken to sort data: %lld\n",(long long)(timer2 -timer1));
+if(argc>2 && (strcmp(argv[2],"-compare")==0 || strcmp(argv[2],"-c")==0)){
+	time(&timer3);
+	std::sort(arr2,arr2+arraySize);	
+	time(&timer4);
+	printf("\nTime taken Inbuilt sort: %lld\n",(long long)(timer4 -timer3));
+}
+    for(i = 1 ; i< arraySize; i++)
+    {
+        if(arr1[i]<arr1[i-1])
+            printf("stack sort did not sort the array!!!!");
+        break;
+    }
+    return 0;
+}
